36a5a089329e81381451d6e0de498d8c56de1443
Darren Davis <darren.davis@twosixtech.com>
2025-10-15 19:10:43 +0000

    Updated file size formatting logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+struct Sizes {
+    bytes: u64,
+    kilobytes: f64,
+    megabytes: f64,
+    gigabytes: f64,
+}
+
+impl Sizes {
+    fn convert(units: u64, unit_type: &str) -> Self {
+        match unit_type {
+            "kb" => Self {
+                bytes: units * 1000,
+                kilobytes: units as f64,
+                megabytes: units as f64 / 1000.0,
+                gigabytes: units as f64 / 1_000_000.0,
+            },
+            "mb" => Self {
+                bytes: units * 1_000_000,
+                kilobytes: units as f64 * 1000.0,
+                megabytes: units as f64,
+                gigabytes: units as f64 / 1000.0,
+            },
+            "gb" => Self {
+                bytes: units * 1_000_000_000,
+                kilobytes: units as f64 * 1_000_000.0,
+                megabytes: units as f64 * 1000.0,
+                gigabytes: units as f64,
+            },
+             _ => panic!("Unknown unit type"),
+        }
+    }
+
+    fn format(&self) -> String {
+        format!(
+            "Sizes {{ bytes: {} bytes, kilobytes: {:.0} kilobytes, megabytes: {:.0} megabytes, gigabytes: {:.0} gigabytes }}",
+            self.bytes, self.kilobytes, self.megabytes, self.gigabytes
+        )
+    }
+}
 
 enum FileSize {
     Bytes(u64),
@@ -22,45 +61,6 @@ fn format_size(size: u64) -> String {
     }
 }
 
-//------------------------------------------------------------
-#[derive(Debug)]
-struct Sizes {
-    bytes: String,
-    kilobytes: String,
-    megabytes: String,
-    gigabytes: String,
-}
-
-impl Sizes {
-    fn convert(units: u64, unit_type: &str) -> Self {
-        match unit_type {
-            "kb" => Self {
-                bytes: format!("{} bytes", units * 1000),
-                kilobytes: format!("{:.0} kilobytes", units as f64),
-                megabytes: format!("{:.0} megabytes", units as f64 / 1000.0),
-                gigabytes: format!("{:.0} gigabytes", units as f64 / 1_000_000.0),
-            },
-            "mb" => {
-                Self {
-                    bytes: format!("{} bytes", units * 1_000_000),
-                    kilobytes: format!("{:.0} kilobytes", units as f64 * 1000.0),
-                    megabytes: format!("{:.0} megabytes", units as f64),
-                    gigabytes: format!("{:.0} gigabytes", units as f64 / 1000.0),
-                }
-            },
-            "gb" => Self {
-                bytes: format!("{} bytes", units * 1_000_000_000),
-                kilobytes: format!("{:.0} kilobytes", units as f64 * 1_000_000.0),
-                megabytes: format!("{:.0} megabytes", units as f64 * 1000.0),
-                gigabytes: format!("{:.0} gigabytes", units as f64),
-            },
-            _ => panic!("Unknown unit type"),
-        }
-    }
-}
-//------------------------------------------------------------
-
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let first_arg = &args[1];
@@ -73,7 +73,8 @@ fn main() {
             match units.parse::<u64>() {
                 Ok(units) => {
                     println!("units: {}, unit_type: {}", units, unit_type);
-                    let sizes = Sizes::convert(units, unit_type.to_lowercase().as_str());
+                    let unit_type = unit_type.to_lowercase();
+                    let sizes = Sizes::convert(units, &unit_type).format();
                     println!("{:?}", sizes)
                 }                   
                 Err(_) => println!("Error: units '{}' is not a valid number", units),
@@ -82,9 +83,6 @@ fn main() {
         // Match any other case (e.g., too few or too many arguments).
         _ => println!("Error: Invalid number of arguments. Expected 'units unit_type'"),
     }
-
-
-    println!("First argument: {}", first_arg);
     
     let result = format_size(6888837399);
     println!("{}", result)
